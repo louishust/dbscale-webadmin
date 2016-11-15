079aaefe56562a2e26ab8a014c65591ce1c44bf1
shuai.lou <louis.hust@gmail.com>
2016-11-15 11:23:01 +0800

    Add show cluster.

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask_login import login_required
 from . import main
+from ..models import Cluster
 
 @main.route('/', methods=['GET'])
 @login_required
@@ -11,9 +12,16 @@ def index():
 @main.route('/sql', methods=['GET'])
 @login_required
 def sql():
-    return render_template('sql.html')
+    clusters = Cluster.query
+    return render_template('sql.html', clusters=clusters)
 
 @main.route('/add_cluster', methods=['GET'])
 @login_required
 def add_cluster():
     return render_template('add_cluster.html')
+
+@main.route('/show_cluster', methods=['GET'])
+@login_required
+def show_cluster():
+    clusters = Cluster.query
+    return render_template('show_cluster.html', clusters=clusters)
